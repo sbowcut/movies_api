886f7082a86edd128c0845553665005219ba08c6
Sam Bowcut <sbowcut@gmail.com>
2021-08-05 13:17:03 -0600

    doDelete

--- src/main/java/MoviesServlet.java
+++ src/main/java/MoviesServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 @WebServlet(name = "MoviesServlet", urlPatterns = "/movies")
@@ -15,17 +16,121 @@ public class MoviesServlet extends HttpServlet {
 
         response.setContentType("application/json");
         try {
+            //get object which can write to the response
             PrintWriter out = response.getWriter();
 
+            //eventually get movies from the database
             Movie movie = new Movie(2, "King King", "1942", "Harry Carey",
                     "Elsa Banks", "234234234", "there aint one", "cheap",
                     "Big Chungus Monkey");
+            //turn into json string
             String movieString = new Gson().toJson(movie);
 
+            //object into response
             out.println(movieString);
 
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+      response.setContentType("application/json");
+
+      PrintWriter out = null;
+      try{
+          out = response.getWriter();
+
+          //get the stream of characters from the request
+          BufferedReader reader = request.getReader();
+
+          //turn stream into an array of movies
+          Movie[] movies = new Gson().fromJson(reader, Movie[].class);
+
+          //sout out properties for each movie so we know they made it
+          for(Movie movie : movies){
+              System.out.println(movie.getId());;
+              System.out.println(movie.getTitle());
+              System.out.println(movie.getDirector());
+              System.out.println(movie.getActors());
+              System.out.println(movie.getGenre());
+              System.out.println(movie.getImdbID());
+              System.out.println(movie.getPlot());
+              System.out.println(movie.getPoster());
+              System.out.println("******************************************");
+          }
+
+
+      }catch(Exception ex){
+          System.out.println(ex.getMessage());
+      }
+
+      //write a meaningful response and set status code to 200
+      out.println(new Gson().toJson("{message: \"Movies Post was successful\"}"));
+      response.setStatus(200);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response){
+
+        response.setContentType("application/json");
+
+        PrintWriter out = null;
+
+        try{
+            out = response.getWriter();
+
+            //get the stream of characters from the request
+            BufferedReader reader = request.getReader();
+
+            //turn stream into an array of movies
+            Movie[] movies = new Gson().fromJson(reader, Movie[].class);
+
+            //sout out properties for each movie so we know they made it
+            for(Movie movie : movies){
+                System.out.println(movie.getId());;
+                System.out.println(movie.getTitle());
+                System.out.println(movie.getDirector());
+                System.out.println(movie.getActors());
+                System.out.println(movie.getGenre());
+                System.out.println(movie.getImdbID());
+                System.out.println(movie.getPlot());
+                System.out.println(movie.getPoster());
+                System.out.println("******************************************");
+            }
+
+
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        //write a meaningful response and set status code to 200
+        out.println(new Gson().toJson("{message: \"Movies PUT was successful\"}"));
+        response.setStatus(200);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response){
+        response.setContentType("application/json");
+
+        PrintWriter out = null;
+
+        try{
+            out = response.getWriter();
+
+            BufferedReader reader = request.getReader();
+
+            int id = new Gson().fromJson(reader, int.class);
+
+            System.out.println("The movie id to delete: " + id);
+
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        out.println(new Gson().toJson("{message: \"Movies DELETE was successful\"}"));
+        response.setStatus(200);
+    }
+
 }
